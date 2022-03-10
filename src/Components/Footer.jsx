30a47dfef71321f7c175a87c0d06dae88ee9ee30
“Annualok” <singhannapurna540@gmail.com>
2022-03-10 20:40:58 +0530

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+function Footer(){
+    return(
+        <div>
+            <h1>Indus Web Solution PVT LTD copyright&copy;2022-2023</h1>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css';
+function Header(){
+    return (
+        <div className='app-container'>
+            <h1>Header</h1>
+        </div>
+    );
+}
+export default Header;
